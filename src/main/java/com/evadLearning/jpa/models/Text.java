b218d6381e225bc562c648a3f0c1675cdecd3d0f
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-03 23:38:44 +0300

    3:05:24

---
+++ src/main/java/com/evadLearning/jpa/models/Text.java
@@ -0,0 +1,14 @@
+package com.evadLearning.jpa.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Data
+public class Text {
+    private String content;
+}
